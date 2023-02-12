0dae8b8ff6b8323d13814175768dabaa82496424
dsalazarl <dsalazarl@gmail.com>
2023-02-12 17:38:10 -0300

    added input handler for main execution

--- compute25.py
+++ compute25.py
@@ -1,9 +1,21 @@
 # compute25.py
 
+import re
+
 def compute25(expr_input: str) -> str:
 	'''Descripción de la función'''
 	return "SIN SOLUCIÓN"
 
 
 if __name__ == "__main__":
-	print('Hola')
+	running = True
+	while running:
+		str_in = input("\nIngrese número de 4 dígitos <\"fin\"> para terminar:\n")
+		if str_in == "fin" or str_in == "FIN":
+			print("Ejecución terminada. Adiós!")
+			running = False
+		else:
+			if bool(re.match("^\d{4}$", str_in)):
+				print(compute25(str_in))
+			else:
+				print("Formato incorrecto!\n")
